836ceb0569bf8b4737a9f1317e6ebe82f60780fc
DucLe1109 <74303018+DucLe1109@users.noreply.github.com>
2021-07-29 04:01:44 +0700

    abc

---
+++ Areas/Admin/Controllers/AdminHomeController.cs
@@ -0,0 +1,30 @@
+﻿using _ExcellOn_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _ExcellOn_.Areas.Admin.Controllers
+{
+    public class AdminHomeController : BaseController
+    {
+        private Entities db = new Entities();
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            string UserName = (string)Session["UserName"];
+            if (UserName == null)
+            {
+                return RedirectToAction("Login","User");
+            }
+            else
+            {
+                var CurrentUser = db.UserInFoes.Where(x => x.User_Name == UserName).FirstOrDefault();
+                ViewBag.CurrentUser = CurrentUser;
+                return View();
+            }
+           
+        }
+    }
+}
---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,99 @@
+﻿using _ExcellOn_.Models;
+using _ExcellOn_.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _ExcellOn_.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private Entities db = new Entities();
+
+        // Function SignUp
+        [HttpPost]
+        public JsonResult SignUp(SignUpViewModel modelSignUp)
+        {
+            if (modelSignUp.UserName != null && modelSignUp.Email != null && modelSignUp.Password != null)
+            {
+                var User = db.Customers.Where(x => x.Customer_UserName == modelSignUp.UserName || x.Customer_Email == modelSignUp.Email).FirstOrDefault();
+                if (User != null)
+                {
+                    string message = "Username or Email is available !";
+                    return Json(message, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    Customer newCustomer = new Customer();
+                    newCustomer.Customer_UserName = modelSignUp.UserName;
+                    newCustomer.Customer_Email = modelSignUp.Email;
+                    newCustomer.Customer_Phone = modelSignUp.Telephone;
+                    newCustomer.Customer_Name = modelSignUp.FullName;
+                    newCustomer.Customer_Password = BCrypt.Net.BCrypt.HashPassword(modelSignUp.Password);
+
+                    newCustomer.Customer_Email = modelSignUp.Email;
+                    db.Customers.Add(newCustomer);
+                    db.SaveChanges();
+                    string message = "Create Successfully !";
+                    return Json(message, JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+            {
+                string message = "Create not successfully !";
+                return Json(message, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        // Function Login View
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View("~/Views/Customer/Login.cshtml");
+        }
+
+        // Function Login
+        [HttpPost]
+        public ActionResult Login(SignInViewModel modelSignIn)
+        {
+            if (modelSignIn.UserName != null && modelSignIn.Password != null)
+            {
+                var Customer = db.Customers.Where(x => x.Customer_UserName == modelSignIn.UserName).FirstOrDefault();
+                if (Customer != null)
+                {
+                    bool test = BCrypt.Net.BCrypt.Verify(modelSignIn.Password, Customer.Customer_Password);
+                    if (test)
+                    {
+                        Session["CustomerName"] = modelSignIn.UserName;
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ViewBag.message = "Account or Password is not correct !";
+                        return View("~/Views/Customer/Login.cshtml");
+                    }
+                }
+                else
+                {
+                    ViewBag.message = "Account or Password is not correct !";
+                    return View("~/Views/Customer/Login.cshtml");
+                }
+            }
+            else
+            {
+                ViewBag.message = "Account or Password is null !";
+                return View("~/Views/Customer/Login.cshtml");
+            }
+        }
+        
+        // Function Logout
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login", "Customer");
+        }
+
+    }
+}
