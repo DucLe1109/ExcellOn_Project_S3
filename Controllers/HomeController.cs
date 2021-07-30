fcd14ed44a26453f5dd6a6c575738e5fa74b0db8
Lê Đức Phúc <58520153+PhucLD-Dev@users.noreply.github.com>
2021-07-30 13:03:25 +0700

    update contact us

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -24,9 +25,39 @@ namespace _ExcellOn_.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+           return View();
+        }
+        [HttpPost]
+        public ViewResult Contact(_ExcellOn_.Models.MailModel _objModelMail)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage mail = new MailMessage();
+                string content = System.IO.File.ReadAllText(Server.MapPath("~/Public/assets/template/neworder.html"));
 
-            return View();
+                content = content.Replace("{{CustomerName}}", _objModelMail.w3lName);
+                content = content.Replace("{{Phone}}", _objModelMail.w3lPhone);
+                content = content.Replace("{{Email}}", _objModelMail.w3lSender);
+                content = content.Replace("{{text}}", _objModelMail.w3lMessage);
+                mail.To.Add("phuchuong120519951@gmail.com");
+                mail.From = new MailAddress(_objModelMail.w3lSender);
+                mail.Subject = _objModelMail.Subject;
+                string Body = _objModelMail.w3lMessage;
+                mail.Body = content;
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential("phuchuong120519951@gmail.com", "phuchuong95"); // Enter seders User name and password  
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+                return View("Contact", _objModelMail);
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
