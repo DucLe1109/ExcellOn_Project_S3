88a7d4ac6542832387794e00906870fc82a9cca3
Lê Đức Phúc <58520153+PhucLD-Dev@users.noreply.github.com>
2021-07-30 10:41:41 +0700

    update contact us

---
+++ Models/MailModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _ExcellOn_.Models
+{
+    public class MailModel
+    {
+        public string w3lSender
+        {
+            get;
+            set;
+        }
+        public string w3lName
+        {
+            get;
+            set;
+        }
+        public string Subject
+        {
+            get;
+            set;
+        }
+        public string w3lMessage
+        {
+            get;
+            set;
+        }
+        public string w3lPhone
+        {
+            get;
+            set;
+        }
+    }
+}
