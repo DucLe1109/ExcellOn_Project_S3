531608c7b03db75e180a4cb13d1321a26d4a9cb4
DucLe1109 <74303018+DucLe1109@users.noreply.github.com>
2021-08-08 21:31:37 +0700

    Thay phân trang ajax bằng loadmore ajax cho giao diện phân công nhân sự.(fix được bug mất checked sau khi load trang mới)

--- Areas/Admin/Controllers/ServiceManagementController.cs
+++ Areas/Admin/Controllers/ServiceManagementController.cs
@@ -37,126 +37,45 @@ namespace _ExcellOn_.Areas.Admin.Controllers
         public ActionResult Assign_Staff(int id)
         {
             OrderDetail ord = db.OrderDetails.Where(x => x.Id == id).FirstOrDefault();
-            List<Staff_OrderDetail> list_sraff_ord = db.Staff_OrderDetail.Where(x => x.OrderDetail_Id == id).ToList();
-            ViewBag.list_sraff_ord = list_sraff_ord;
+            List<Staff_OrderDetail> list_id_staff_ord = db.Staff_OrderDetail.Where(x => x.OrderDetail_Id == id).ToList();
+            List<Staff> list_staff_this_ord = new List<Staff>();
+            foreach (var item in list_id_staff_ord) {
+                Staff _staff = db.Staffs.Where(x => x.Id == item.Staff_Id).FirstOrDefault();
+                list_staff_this_ord.Add(_staff);
+            }
+            int list_staff_this_ord_count = list_staff_this_ord.Count;
+            
+            if (list_staff_this_ord_count < 21)
+            {
+                int list_staff_free_add_count = 21 - list_staff_this_ord_count;
+                List<Staff> list_staff_free = orderDetail_Function.Take_List_Staff_Free(id);
+                List<Staff> list_staff_append = new List<Staff>();
+                if (list_staff_free.Count >= list_staff_free_add_count)
+                {
+                    var _list_staff_free = list_staff_free.Take(list_staff_free_add_count);
+                    foreach (var item in _list_staff_free)
+                    {
+                        list_staff_append.Add(item);
+                    }
+                }
+                ViewBag.list_staff_this_ord = list_staff_this_ord;
+                ViewBag.list_staff_append = list_staff_append;
+                return View(ord);
+            }
+            ViewBag.list_staff_this_ord = list_staff_this_ord;
             return View(ord);
         }
 
-        //
-        public JsonResult GetTotalPage(int items, int ord_id)
+        [HttpGet]
+        public JsonResult LoadMore(int ord_id, int list_staff_was_append_count)
         {
-            // Take list_staff_free
-            var list_staff_free_for_thisOrd = orderDetail_Function.Take_List_Staff_Free(ord_id);
-            // End Take list_staff_free
+            AppendedStaff list_appendedStaff = new AppendedStaff();
 
-            // take total page
-            int p;
-            var list_staff_free_for_thisOrd_Count = list_staff_free_for_thisOrd.Count;
-            float pages_tamp = (float)list_staff_free_for_thisOrd_Count / items;
-            float pages_tamp2 = (int)pages_tamp;
-            pages_tamp2 = (float)(pages_tamp2 + 0.00000000001);
-            if (pages_tamp > pages_tamp2)
-            {
-                p = (int)pages_tamp2 + 1;
-            }
-            else
-            {
-                p = (int)pages_tamp2;
-            }
-            // end take total page
-
-            return Json(p, JsonRequestBehavior.AllowGet);
-        }
-
-        public JsonResult GetItem(GetItemObj getItemObj)
-        {
-            if (getItemObj.list_staff_id_checked != null)
-            {
-                List<int> list_staff_id_checked = getItemObj.list_staff_id_checked;
-                List<int> list_staff_id_checked_session = (List<int>)Session["list_staff_id_checked"];
-
-                // Th1: chưa có session thì khởi tạo session mới
-                if (list_staff_id_checked_session == null)
-                {
-                    List<int> list_staff_id_checked_new_session = new List<int>();
-                    foreach (var item in list_staff_id_checked)
-                    {
-                        list_staff_id_checked_new_session.Add(item);
-                    }
-                    Session["list_staff_id_checked"] = list_staff_id_checked_new_session;
-                }
-                // Th2: có session rồi thì chia 2 trường hợp là thêm các checked hoặc bớt các checked
-                else
-                {
-                    // TH1:  request bỏ các checked (bớt)
-                    if (list_staff_id_checked_session.Count > list_staff_id_checked.Count)
-                    {
-                        List<int> list_staff_id_checked_session_haveto_delete = new List<int>();
-                        foreach (var item in list_staff_id_checked_session)
-                        {
-                            if (list_staff_id_checked.Contains(item) == false)
-                            {
-                                list_staff_id_checked_session_haveto_delete.Add(item);
-                            }
-                        }
-                        foreach (var item in list_staff_id_checked_session_haveto_delete)
-                        {
-                            if (list_staff_id_checked_session.Contains(item))
-                            {
-                                list_staff_id_checked_session.Remove(item);
-                            }
-                        }
-                        Session["list_staff_id_checked"] = list_staff_id_checked_session;
-                        
-                    }
-                    // TH2: request có các checked mới (thêm)
-                    else if (list_staff_id_checked_session.Count < list_staff_id_checked.Count)
-                    {
-                        foreach (var item in list_staff_id_checked)
-                        {
-                            if (list_staff_id_checked_session.Contains(item) == false)
-                            {
-                                list_staff_id_checked_session.Add(item);
-                            }
-                        }
-                        Session["list_staff_id_checked"] = list_staff_id_checked_session;
-                    }
-                    else if (list_staff_id_checked_session.Count == list_staff_id_checked.Count)
-                    {
-                        foreach (var item in list_staff_id_checked_session)
-                        {
-                            list_staff_id_checked_session.Remove(item);
-                        }
-                        foreach (var item in list_staff_id_checked)
-                        {
-                            list_staff_id_checked_session.Add(item);
-                        }
-                        Session["list_staff_id_checked"] = list_staff_id_checked_session;
-                    }
-                }
-            }
-
-            int current_page = getItemObj.current_page;
-            int ord_id = getItemObj.ord_id;
-
-            // Take all staff free for this ord 
-            List<Staff> list_staff_free = orderDetail_Function.Take_List_Staff_Free(ord_id);
-            // End 
-
-            // Take all staff got assigned for this ord
-            List<Staff_OrderDetail> list_id_staff_of_ord = db.Staff_OrderDetail.Where(x => x.OrderDetail_Id == ord_id).ToList();
-            foreach (var item in list_id_staff_of_ord)
-            {
-                Staff staff = db.Staffs.Where(x => x.Id == item.Staff_Id).FirstOrDefault();
-                // Add staff got assigned in first's list
-                list_staff_free.Insert(0, staff);
-            }
-            // End
-
-            int offset = (current_page - 1) * 21;
-            var list_staff = (from x in list_staff_free select x).Skip(offset).Take(21);
-            List<StaffViewModel> list_staff_vm = new List<StaffViewModel>();
-            foreach (var item in list_staff)
+            OrderDetail ord = db.OrderDetails.Where(x => x.Id == ord_id).FirstOrDefault();
+            List<Staff> list_staff_free = orderDetail_Function.Take_List_Staff_Free(ord.Id);
+            var list_staff_append = (from x in list_staff_free select x).Skip(list_staff_was_append_count).Take(21);
+            List<StaffViewModel> list_staff_vmd_free_append = new List<StaffViewModel>();
+            foreach (var item in list_staff_append)
             {
                 StaffViewModel _new = new StaffViewModel();
                 _new.Id = item.Id;
@@ -172,14 +91,171 @@ namespace _ExcellOn_.Areas.Admin.Controllers
                 _new.Staff_Phone = item.Staff_Phone;
                 _new.Staff_Status = item.Staff_Status;
                 _new.Staff_UserName = item.Staff_UserName;
-                list_staff_vm.Add(_new);
+                list_staff_vmd_free_append.Add(_new);
             }
 
-            GetItemObj_Return getItemObj_Return = new GetItemObj_Return();
-            getItemObj_Return.list_staff = list_staff_vm;
-            getItemObj_Return.list_staff_id_checked = (List<int>)Session["list_staff_id_checked"];
-            return Json(getItemObj_Return, JsonRequestBehavior.AllowGet);
+            if (list_staff_vmd_free_append.Count < 21)
+            {
+                list_appendedStaff.isLoadMore = false;
+                list_appendedStaff.list_staff_append = list_staff_vmd_free_append;
+            }
+            else
+            {
+                list_appendedStaff.isLoadMore = true;
+                list_appendedStaff.list_staff_append = list_staff_vmd_free_append;
+            }
+            return Json(list_appendedStaff,JsonRequestBehavior.AllowGet);
         }
+        //
+        //public JsonResult GetTotalPage(int items, int ord_id)
+        //{
+        //    // Take list_staff_free
+        //    var list_staff_free_for_thisOrd = orderDetail_Function.Take_List_Staff_Free(ord_id);
+        //    // End Take list_staff_free
+
+        //    // take total page
+        //    int p;
+        //    var list_staff_free_for_thisOrd_Count = list_staff_free_for_thisOrd.Count;
+        //    float pages_tamp = (float)list_staff_free_for_thisOrd_Count / items;
+        //    float pages_tamp2 = (int)pages_tamp;
+        //    pages_tamp2 = (float)(pages_tamp2 + 0.00000000001);
+        //    if (pages_tamp > pages_tamp2)
+        //    {
+        //        p = (int)pages_tamp2 + 1;
+        //    }
+        //    else
+        //    {
+        //        p = (int)pages_tamp2;
+        //    }
+        //    // end take total page
+
+        //    return Json(p, JsonRequestBehavior.AllowGet);
+        //}
+
+        //public JsonResult GetItem(GetItemObj getItemObj)
+        //{
+        //    if (getItemObj.list_staff_id_checked != null)
+        //    {
+        //        List<int> list_staff_id_checked = getItemObj.list_staff_id_checked;
+        //        List<int> list_staff_id_checked_session = (List<int>)Session["list_staff_id_checked"];
+
+        //        // Th1: chưa có session thì khởi tạo session mới
+        //        if (list_staff_id_checked_session == null)
+        //        {
+        //            List<int> list_staff_id_checked_new_session = new List<int>();
+        //            foreach (var item in list_staff_id_checked)
+        //            {
+        //                list_staff_id_checked_new_session.Add(item);
+        //            }
+        //            Session["list_staff_id_checked"] = list_staff_id_checked_new_session;
+        //        }
+        //        // Th2: có session rồi thì chia 2 trường hợp là thêm các checked hoặc bớt các checked
+        //        else
+        //        {
+
+        //            // TH1:  request bỏ các checked (bớt), hoặc check thêm từ trang mới.
+        //            if (list_staff_id_checked_session.Count > list_staff_id_checked.Count)
+        //            {
+        //                List<int> list_staff_id_checked_session_haveto_delete_or_add = new List<int>();
+        //                foreach (var item in list_staff_id_checked_session)
+        //                {
+        //                    if (list_staff_id_checked.Contains(item) == false)
+        //                    {
+        //                        list_staff_id_checked_session_haveto_delete_or_add.Add(item);
+        //                    }
+        //                }
+        //                if (list_staff_id_checked_session_haveto_delete_or_add.Count == list_staff_id_checked_session.Count && list_staff_id_checked_session_haveto_delete_or_add.Except(list_staff_id_checked_session).Any() == false)
+        //                {
+        //                    foreach (var item in list_staff_id_checked)
+        //                    {
+        //                        list_staff_id_checked_session.Add(item);
+        //                    }
+        //                }
+        //                else
+        //                {
+        //                    foreach (var item in list_staff_id_checked_session_haveto_delete_or_add)
+        //                    {
+        //                        if (list_staff_id_checked_session.Contains(item))
+        //                        {
+        //                            list_staff_id_checked_session.Remove(item);
+        //                        }
+        //                    }
+        //                }
+
+
+        //                Session["list_staff_id_checked"] = list_staff_id_checked_session;
+
+        //            }
+        //            // TH2: request có các checked mới (thêm)
+        //            else if (list_staff_id_checked_session.Count < list_staff_id_checked.Count)
+        //            {
+        //                foreach (var item in list_staff_id_checked)
+        //                {
+        //                    if (list_staff_id_checked_session.Contains(item) == false)
+        //                    {
+        //                        list_staff_id_checked_session.Add(item);
+        //                    }
+        //                }
+        //                Session["list_staff_id_checked"] = list_staff_id_checked_session;
+        //            }
+        //            else if (list_staff_id_checked_session.Count == list_staff_id_checked.Count)
+        //            {
+
+        //                list_staff_id_checked_session.Clear();
+
+        //                foreach (var item in list_staff_id_checked)
+        //                {
+        //                    list_staff_id_checked_session.Add(item);
+        //                }
+        //                Session["list_staff_id_checked"] = list_staff_id_checked_session;
+        //            }
+        //        }
+        //    }
+
+        //    int current_page = getItemObj.current_page;
+        //    int ord_id = getItemObj.ord_id;
+
+        //    // Take all staff free for this ord 
+        //    List<Staff> list_staff_free = orderDetail_Function.Take_List_Staff_Free(ord_id);
+        //    // End 
+
+        //    // Take all staff got assigned for this ord
+        //    List<Staff_OrderDetail> list_id_staff_of_ord = db.Staff_OrderDetail.Where(x => x.OrderDetail_Id == ord_id).ToList();
+        //    foreach (var item in list_id_staff_of_ord)
+        //    {
+        //        Staff staff = db.Staffs.Where(x => x.Id == item.Staff_Id).FirstOrDefault();
+        //        // Add staff got assigned in first's list
+        //        list_staff_free.Insert(0, staff);
+        //    }
+        //    // End
+
+        //    int offset = (current_page - 1) * 21;
+        //    var list_staff = (from x in list_staff_free select x).Skip(offset).Take(21);
+        //    List<StaffViewModel> list_staff_vm = new List<StaffViewModel>();
+        //    foreach (var item in list_staff)
+        //    {
+        //        StaffViewModel _new = new StaffViewModel();
+        //        _new.Id = item.Id;
+        //        _new.ServiceId = item.ServiceId;
+        //        _new.Staff_AboutMe = item.Staff_AboutMe;
+        //        _new.Staff_Address = item.Staff_Address;
+        //        _new.Staff_Age = item.Staff_Age;
+        //        _new.Staff_Avatar = item.Staff_Avatar;
+        //        _new.Staff_Email = item.Staff_Email;
+        //        _new.Staff_FullName = item.Staff_FullName;
+        //        _new.Staff_Gender = item.Staff_Gender;
+        //        _new.Staff_Password = item.Staff_Password;
+        //        _new.Staff_Phone = item.Staff_Phone;
+        //        _new.Staff_Status = item.Staff_Status;
+        //        _new.Staff_UserName = item.Staff_UserName;
+        //        list_staff_vm.Add(_new);
+        //    }
+
+        //    GetItemObj_Return getItemObj_Return = new GetItemObj_Return();
+        //    getItemObj_Return.list_staff = list_staff_vm;
+        //    getItemObj_Return.list_staff_id_checked = (List<int>)Session["list_staff_id_checked"];
+        //    return Json(getItemObj_Return, JsonRequestBehavior.AllowGet);
+        //}
 
         public JsonResult SubmitAssignment(AssignmentSubmitViewModel request)
         {
